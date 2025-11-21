967229179edc2d8e8fbf5a74f89955ab23bb9c83
Lee Wang <leeeee@gmail.com>
2025-11-21 15:38:25 -0800

    adds templates for view page - wip

--- app.js
+++ app.js
@@ -5,17 +5,26 @@ const { readFileSync } = require('fs')
 const app = express()
 
 const htmlTemplate = '' + readFileSync('./public/template.html')
+const peopleTemplate = '' + readFileSync('./public/people.html')
 const port = 3000
 
 app.use(express.static('./public'))
+app.use(express.json())
 
 app.get('/planets', (req, res) => {
   let output = htmlTemplate.replaceAll(/{%RESOURCE_TYPE%}/g, 'planets')
   res.status(200).end(output)
 })
+
 app.get('/people', (req, res) => {
   let output = htmlTemplate.replaceAll(/{%RESOURCE_TYPE%}/g, 'people')
   res.status(200).end(output)
+})
+
+app.get('/people/:userId', (req, res) => {
+  //let output = htmlTemplate.replaceAll(/{%RESOURCE_TYPE%}/g, 'people')
+  let output = 
+  res.status(200).end('hello')
 })
 
 app.get('/', (req, res) => {
--- public/api.js
+++ public/api.js
@@ -1,15 +1,24 @@
 const BASE_URL = 'https://www.swapi.tech/api';
 
+function callGet(resourceType, uid) {
+}
 
 // might need local cors proxy node module?
-function callApi(resourceType) {
+function callSearch(resourceType) {
   const searchTerm = document.getElementById('search').value
   const results = document.getElementById('results')
+  results.textContent = ''
   console.log("Calling API on " + resourceType)
   let url = `${BASE_URL}/${resourceType}?name=${searchTerm}`
   fetch(url)
     .then(response => response.json())
     .then(responseJson => {
       console.log(responseJson.result)
+      responseJson.result.forEach((record) => {
+        console.log(record.uid)
+        const textNode = document.createElement('li')
+        textNode.innerHTML = `${record.properties.name} (<a href='${resourceType}/${record.uid}'>details</a>)`
+        results.append(textNode)
+      })
     })
 }
