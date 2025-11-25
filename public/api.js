a398e7b484c9afe135bcc8be5fbd6ef37f99a0ed
Lee Wang <leeeee@gmail.com>
2025-11-25 13:16:56 -0800

    refactoring api js and minor styling/ux improvements

--- public/api.js
+++ public/api.js
@@ -28,25 +28,49 @@ function callGet(resourceType, uid, properties = []) {
     })
 }
 
-function callSearch(resourceType) {
-  const searchTerm = document.getElementById('search').value
-  const results = document.getElementById('results')
+function showError(message) {
   const errorDiv = document.getElementById('error-message')
-  results.textContent = ''
+  errorDiv.style.visibility = 'visible'
+  errorDiv.innerHTML = message
+}
+
+function clearSearch() {
+  document.getElementById('search').value = ''
+}
+
+async function callSearch(resourceType) {
+  const searchTerm = document.getElementById('search').value
+
+  if (searchTerm === '') {
+    showError('Please specify a search term')
+    return
+  }
+
+  const resultsNode = document.getElementById('results')
+  resultsNode.textContent = ''
 
   const url = buildUrl(resourceType, { name: searchTerm })
-  fetch(url)
-    .then(response => response.json())
-    .then(responseJson => {
-      if (responseJson.result.length > 0) {
-        responseJson.result.forEach((record) => {
-          const textNode = document.createElement('p')
-          textNode.innerHTML = `${record.properties.name} (<a href='${resourceType}/${record.uid}'>details</a>)`
-          results.append(textNode)
-        })
-      } else {
-        errorDiv.innerHTML = 'No results!'
-        errorDiv.style.visibility = 'visible'
-      }
-    })
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      console.log(response)
+      showError(`Couldn't fetch: ${response.status}`)
+      return
+    }
+    const responseJson = await response.json()
+    const results = responseJson.result
+
+    if (results.length > 0) {
+      results.forEach((record) => {
+        const textNode = document.createElement('p')
+        textNode.innerHTML = `${record.properties.name} (<a href='${resourceType}/${record.uid}'>details</a>)`
+        resultsNode.append(textNode)
+      })
+    } else {
+      showError('No results found!')
+    }
+  } catch (error) {
+    console.log(error)
+    showError(`Something went wrong with the fetch: ${error}. The server might not be reachable`)
+  }
 }
